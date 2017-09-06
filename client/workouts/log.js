114e9fe635ad724dd2f83cddc9f3e119dffce539
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-06 17:03:49 -0400

    Added remove function for history

--- client/workouts/log.js
+++ client/workouts/log.js
@@ -12,12 +12,20 @@ $(function(){
 				$("#log-definition").children().remove();
 				$("#log-definition").append(opts);
 			},
-			setHistory: function(){
+			setHistory: function() {
 				var history = WorkoutLog.log.workouts;
 				var len = history.length;
 				var lis = "";
-				for (var i = 0; i < len; i++){
-					lis += "<li class='list-group-item'>" + history[i].def + ' - ' + history[i].result + "</li>";
+					for (var i = 0; i < len; i++) {
+					lis += "<li class='list-group-item'>" + 
+					// history[i].id + " - " + 
+					history[i].def + " - " + 
+					history[i].result + " " +
+					// pass the log.id into the button's id attribute // watch your quotes!
+					"<div class='pull-right'>" +
+						"<button id='" + history[i].id + "' class='update'><strong>U</strong></button>" +
+						"<button id='" + history[i].id + "' class='remove'><strong>X</strong></button>" +
+					"</div></li>";
 				}
 				$("#history-list").children().remove();
 				$("#history-list").append(lis);
@@ -26,7 +34,7 @@ $(function(){
 				var itsLog = {
 					desc: $("#log-description").val(),
 					result: $("#log-result").val(),
-					def: $("#log-definition option: selected").text()
+					def: $("#log-definition option:selected").text()
 				};
 				var postData = { log: itsLog };
 				var logger = $.ajax({
@@ -57,11 +65,40 @@ $(function(){
 				.fail(function(err){
 					console.log(err);
 				});
+			},
+			delete: function(){
+				var thisLog = {
+					//'this' is the button on the li
+					//.attr('id') targets the value of the id attribute of button
+					id: $(this).attr("id")
+				};
+				var deleteData = { log: thisLog };
+				var deleteLog = $.ajax({
+					type: "DELETE",
+					url: WorkoutLog.API_BASE + "log",
+					data: JSON.stringify(deleteData),
+					contentType: "application/json"
+				});
+
+				//removes list item
+				//references button then grabs closest li
+				$(this).closest("li").remove();
+
+				//deletes item out of workouts array
+				for (var i = 0; i < WorkoutLog.log.workouts.length; i++){
+					if (WorkoutLog.log.workouts[i].id == thisLog.id){
+						WorkoutLog.log.workouts.splice(i, 1);
+					}
+				}
+				deleteLog.fail(function(){
+					console.log("nope, you dind't delete it.");
+				});
 			}
 		}
 	});
 	//click the button and create a log entry
 	$("#log-save").on("click", WorkoutLog.log.create);
+	$("#history-list").delegate(".remove", "click", WorkoutLog.log.delete);
 
 	if (window.localStorage.getItem("sessionToken")){
 		WorkoutLog.log.fetchAll();
