b39c09a54a74f99ca12cff15484631aa9bbe51df
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-06 09:16:05 -0400

    Signup setup

--- client/app.js
+++ client/app.js
@@ -1,16 +1,73 @@
-$(document).ready(function(){
-	$('#testAPI').on('click', function(){
-		console.log('working!!!');
+$(function(){
+	var WorkoutLog = (function($, undefined){
+		var API_BASE = "http://localhost:3000/api/";
+		var userDefinitions = []
+
+		var setAuthHeader = function(sessionToken){
+			window.localStorage.setItem("sessionToken", sessionToken);
+			//Set the authorization header
+			//This can be done on individual calls
+			//Here, we showcase ajaxSetup as a global tool
+			$.ajaxSetup({
+				"headers": {
+					"Authorization": sessionToken
+				}
+			});
+		};
+
+		//public
+		return {
+			API_BASE: API_BASE,
+			setAuthHeader: setAuthHeader
+		};
+
+	})(jQuery);
+
+	//Ensure .disabled aren't clickable
+	$(".nav-tabs a[data-toggle=tab]").on("click", function(e){
+		if ($(this).hasClass("disabled") && !token){
+			e.preventDefault();
+			return false;
+		}
 	});
 
-	var test = $.ajax({
-		type: 'GET',
-		url: 'http://localhost:3000/api/test'
-	})
-	.done(function(data){
-		console.log(data);
-	})
-	.fail(function(){
-		console.log('Oh no!');
+	//bind enter key
+	$(document).on("keypress", function(e){
+		if (e.which === 13){
+			if ($("#signup-modal").is(":visible")){
+				$("#signup").trigger("click");
+			} 
+			if ($("#login-modal").is(":visible")){
+				$("#login").trigger("click");
+			}
+		}
 	});
+	//setHeader if we
+	var token = window.localStorage.getItem("sessionToken");
+	if (token){
+		WorkoutLog.setAuthHeader(token);
+	}
+
+	//expose this to the other workoutlog modules
+	window.WorkoutLog = WorkoutLog;
 });
+
+
+
+
+// $(document).ready(function(){
+// 	$('#testAPI').on('click', function(){
+// 		console.log('working!!!');
+// 	});
+
+// 	var test = $.ajax({
+// 		type: 'GET',
+// 		url: 'http://localhost:3000/api/test'
+// 	})
+// 	.done(function(data){
+// 		console.log(data);
+// 	})
+// 	.fail(function(){
+// 		console.log('Oh no!');
+// 	});
+// });
